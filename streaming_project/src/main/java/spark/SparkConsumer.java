0d0a8acef871ea9b31ece477c6fd2ada97d39de6
Hadhami <hadhami.mat@gmail.com>
2016-03-23 19:13:46 +0100

    spark consummer with sql request

--- streaming_project/src/main/java/spark/SparkConsumer.java
+++ streaming_project/src/main/java/spark/SparkConsumer.java
@@ -4,57 +4,73 @@ package spark;
  * Created by Hadhami on 10/03/2016.
  */
 
-import kafka.serializer.StringDecoder;
 import models.Consommation;
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.VoidFunction;
+
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaPairInputDStream;
-import org.apache.spark.streaming.api.java.JavaPairReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka.*;
+import kafka.serializer.StringDecoder;
+
 import scala.Tuple2;
+
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.SQLContext;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
+
 
 public class SparkConsumer {
 
     static public String brokerList = "ambari:6667,slave1:6667";
 
     public static void main(String[] args){
-        //create spark config
+        
+        //Create spark config
         SparkConf sparkConf = new SparkConf().setAppName("kafkaStreaming");
         sparkConf = new SparkConf().setMaster("local[*]").setAppName("kafkaStreaming");
-        //crete spark context for each batch
+        //Create spark context for each batch
         JavaSparkContext jsc = new JavaSparkContext(sparkConf);
         //create spark streaming context
         JavaStreamingContext jssc = new JavaStreamingContext(jsc, Durations.seconds(1));
+        //Create sql context
+        final SQLContext sqlContext = new SQLContext(jsc);
 
-        //read data from kafka topic to direct stream ---->  RDD group
+        //Read data from kafka topic to direct stream ---->  RDD group
         HashSet<String> topicsSet = new HashSet<String>();
         topicsSet.add("topic_edf");
         HashMap<String, String> kafkaParams = new HashMap<String, String>();
         kafkaParams.put("metadata.broker.list",brokerList);
         JavaPairInputDStream<String, String> kafkaStream = KafkaUtils.createDirectStream(jssc, String.class, String.class, StringDecoder.class,StringDecoder.class, kafkaParams, topicsSet);
 
-        //for each rdd
+        //For each rdd
         kafkaStream.foreachRDD(new VoidFunction<JavaPairRDD<String, String>>() {
             public void call(JavaPairRDD<String, String> stringStringJavaPairRDD) throws Exception {
+
+                //Creation de RDD d'objets Consommation
                 JavaRDD<Consommation> consommationJavaRDD = stringStringJavaPairRDD.map(new Function<Tuple2<String, String>, Consommation>() {
                     public Consommation call(Tuple2<String, String> stringStringTuple2) throws Exception {
                         return new Consommation(stringStringTuple2._2());
                     }
                 });
 
-                // requete SQL
-                System.out.println("$$$$$$$$");
-                System.out.println(consommationJavaRDD.collect().toString());
+                //System.out.println(consommationJavaRDD.collect().toString());
+
+                // Apply a schema to an RDD of JavaBeans and register it as a table.
+                DataFrame frame = sqlContext.createDataFrame(consommationJavaRDD,Consommation.class);
+                frame.registerTempTable("Consommation");
+
+                // SQL Request
+                DataFrame consommateurs = sqlContext.sql("SELECT ville,sum(conso) as totalConso FROM Consommation group by ville ");
+                consommateurs.show();
 
             }
         });
